8fba6dae30c76d457a98e5e486b7dfbc1f192f58
winetree94 <winetree94@outlook.com>
2021-10-03 13:44:59 +0900

    feat: init sidebar layout

---
+++ src/components/sidebar/Sidebar.svelte
@@ -0,0 +1,10 @@
+<script type="ts">
+</script>
+
+<div id="side-bar-root">sidebar</div>
+
+<style type="scss">
+  #side-bar-root {
+    width: 200px;
+  }
+</style>
--- src/routes/App.svelte
+++ src/routes/App.svelte
@@ -1,21 +1,45 @@
 <script type="ts">
   import Toolbar from '@/components/toolbar/Toolbar.svelte';
+  import Sidebar from '@/components/sidebar/Sidebar.svelte';
 </script>
 
 <div id="app-root">
-  <Toolbar />
-  <div id="app-body">app</div>
+  <div id="toolbar-root">
+    <Toolbar />
+  </div>
+  <div id="app-body">
+    <div id="side-bar-root">
+      <Sidebar />
+    </div>
+    <div id="app-root">app</div>
+    <div id="property-bar-root">right side bar</div>
+  </div>
 </div>
 
 <style lang="scss">
   #app-root {
     width: 100%;
-    height: 100vh;
     display: flex;
     flex-direction: column;
 
     #app-body {
       flex: 1 1 auto;
+      display: flex;
+      height: calc(100vh - 38px);
+
+      #side-bar-root {
+        flex: 0 0 auto;
+        width: 200px;
+      }
+
+      #app-root {
+        flex: 1 1 auto;
+      }
+
+      #property-bar-root {
+        flex: 0 0 auto;
+        width: 200px;
+      }
     }
   }
 </style>
