a8fa68e5c22e842702d3868390e4ff4b90f73a93
winetree94 <winetree94@outlook.com>
2021-10-03 13:27:15 +0900

    fix: app layout

--- src/routes/App.svelte
+++ src/routes/App.svelte
@@ -2,8 +2,20 @@
   import Toolbar from '@/components/toolbar/Toolbar.svelte';
 </script>
 
-<Toolbar />
-<main>hello world!</main>
+<div id="app-root">
+  <Toolbar />
+  <div id="app-body">app</div>
+</div>
 
 <style lang="scss">
+  #app-root {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+
+    #app-body {
+      flex: 1 1 auto;
+    }
+  }
 </style>
