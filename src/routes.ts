1dfcd8a74d3596d83aad6d8a29b2218e0280783e
duchangkim <duchang.dev@gmail.com>
2021-10-03 22:34:58 +0900

    Feat: Add redirect route wrap

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import { wrap } from 'svelte-spa-router/wrap';
 import Home from '@/routes/Home.svelte';
 import SignUp from '@/routes/SignUp.svelte';
 import SignIn from '@/routes/SignIn.svelte';
@@ -5,8 +6,16 @@ import Edit from '@/routes/Edit.svelte';
 import NotFound from '@/routes/NotFound.svelte';
 import App from '@/routes/App.svelte';
 import Web from '@/routes/Web.svelte';
+import { replace } from 'svelte-spa-router';
 
 export const rootRoute = {
+  '/': wrap({
+    component: NotFound,
+    conditions: [() => {
+      void replace('/home');
+      return true;
+    },]
+  }),
   '/home': Web,
   '/home/*': Web,
   '/app': App,
