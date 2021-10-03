72a78f3729b7107d16a8c38cee22e2933818fa3e
winetree94 <winetree94@outlook.com>
2021-10-03 11:25:08 +0900

    chore: remove unnecessary

--- src/routes/App.svelte
+++ src/routes/App.svelte
@@ -1,17 +1,9 @@
 <script type="ts">
-  import Router from 'svelte-spa-router';
   import Toolbar from '@/components/toolbar/Toolbar.svelte';
-  import Home from '@/routes/Home.svelte';
-
-  const prefix = '/hello';
-  const rootRoute = {
-    '/asdf': Home,
-  };
 </script>
 
 <Toolbar />
 <main>hello world!</main>
-<Router routes={rootRoute} {prefix} />
 
 <style lang="scss">
 </style>
