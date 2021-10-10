4e5550e7065dc023f005c63f6a421115d8750bea
winetree94 <winetree94@outlook.com>
2021-10-10 20:55:24 +0900

    feat: fix subtoolbar layout

--- src/components/toolbar/SubToolbar.svelte
+++ src/components/toolbar/SubToolbar.svelte
@@ -1,7 +1,19 @@
+<script type="ts">
+  let current = 1;
+</script>
+
 <div id="subtoolbar">
-  <div>a</div>
-  <div>b</div>
-  <div>c</div>
+  <div class="subtoolbar-item btn prev">
+    <button>prev</button>
+  </div>
+  <div class="subtoolbar-item">1</div>
+  <div class="subtoolbar-item">2</div>
+  <div class="subtoolbar-item">3</div>
+  <div class="subtoolbar-item">4</div>
+  <div class="subtoolbar-item">5</div>
+  <div class="subtoolbar-item btn prev">
+    <button>next</button>
+  </div>
 </div>
 
 <style type="scss">
@@ -10,5 +22,9 @@
     justify-content: center;
     align-items: center;
     border-bottom: 1px solid rgb(0 0 0 / 10%);
+
+    .subtoolbar-item {
+      margin: 10px;
+    }
   }
 </style>
