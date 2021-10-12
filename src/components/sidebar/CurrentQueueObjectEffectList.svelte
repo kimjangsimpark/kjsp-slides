3eccfe42a6ae8089f052bc3d0c866cb322a1b978
duchangkim <duchang.dev@gmail.com>
2021-10-12 23:21:26 +0900

    feat: Add effect list in sidebar

---
+++ src/components/sidebar/CurrentQueueObjectEffectList.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import type { QueueObjectState } from '@/store/queueObject';
+
+  export let currentQueueObject: QueueObjectState;
+
+  $: currentQueueObjectEffectList = currentQueueObject.effect;
+</script>
+
+<article class="effect-list-wrapper">
+  <header>
+    <h2>Effect List</h2>
+    <button>+</button>
+  </header>
+  <ol class="effect-list">
+    {#each currentQueueObjectEffectList as action}
+      <li class="effect-list-item">{action.type}</li>
+    {/each}
+  </ol>
+</article>
+
+<style lang="scss" scoped>
+  .effect-list-wrapper {
+    padding: {
+      top: 20px;
+      bottom: 20px;
+    }
+
+    header {
+      position: relative;
+      display: flex;
+      justify-content: center;
+
+      h2 {
+        text-align: center;
+        font: {
+          size: 1rem;
+          weight: normal;
+        }
+      }
+
+      button {
+        width: 30px;
+        position: absolute;
+        right: 0;
+      }
+    }
+
+    .effect-list {
+      min-height: 150px;
+      height: 150px;
+      padding: 1rem;
+      list-style: none;
+      overflow-y: auto;
+      box-shadow: inset -1px -5px 6px $gray-line-1;
+
+      .effect-list-item {
+        padding: 0.4rem 0;
+      }
+    }
+  }
+</style>
--- src/components/sidebar/PropertiesSidebar.svelte
+++ src/components/sidebar/PropertiesSidebar.svelte
@@ -1,5 +1,6 @@
 <script type="ts">
   import CurrentQueueObjectActionList from './CurrentQueueObjectActionList.svelte';
+  import CurrentQueueObjectEffectList from './CurrentQueueObjectEffectList.svelte';
   import { QueueObjectState, currentQueObject$ } from '@/store/queueObject';
 
   let currentQueueObject: QueueObjectState;
@@ -10,7 +11,7 @@
 
 <aside id="property-bar-root">
   <CurrentQueueObjectActionList {currentQueueObject} />
-  <article class="current-effects-wrapper">effects</article>
+  <CurrentQueueObjectEffectList {currentQueueObject} />
   <article class="border-wrapper">border</article>
   <article class="fill-wrapper">fill</article>
 </aside>
@@ -21,8 +22,8 @@
     width: 200px;
     border-left: 1px solid $gray-line-1;
 
-    & :global(article) {
-      border-bottom: 1px solid $gray-line-2;
-    }
+    // & :global(article) {
+    //   border-bottom: 1px solid $gray-line-2;
+    // }
   }
 </style>
