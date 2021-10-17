f31e6d1678f3396e584a02b4f130d2e40ff7e624
winetree94 <winetree94@outlook.com>
2021-10-18 06:23:55 +0900

    feat: connect fetcher to app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import '@/styles/global.scss';
 import App from '@/App.svelte';
+import { fetcher } from './misc/fetcher';
+
+fetcher.setBaseUrl('https://some/url/to/start');
+fetcher.setDefaultHeader({
+  'Authorization': 'SOME TOKEN TO USE',
+});
+fetcher.addResponseInterceptor((response) => {
+  if (response.status === 401) {
+    console.log('Unauthorized Reqeust, Please Redirect to login page');
+  }
+});
 
 const app = new App({
   target: document.body,
