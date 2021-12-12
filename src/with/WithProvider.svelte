333178e6c013e303c78215309b6137bc9456b827
winetree94 <winetree94@outlook.com>
2021-12-12 13:25:33 +0900

    chore: add provider component

---
+++ src/with/WithProvider.svelte
@@ -0,0 +1,14 @@
+<script type="ts" context="module">
+  export interface ProviderType<V = any> {
+    name: string;
+    value: V;
+  }
+</script>
+
+<script type="ts">
+  import { setContext } from 'svelte';
+  export let provide: ProviderType;
+  setContext<ProviderType>(provide.name, provide);
+</script>
+
+<slot />
