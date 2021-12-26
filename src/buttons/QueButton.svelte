025b31b9602c25c828471c0157dc8aa4b1a03954
winetree94 <winetree94@outlook.com>
2021-12-26 19:12:20 +0900

    update: subtool features

---
+++ src/buttons/QueButton.svelte
@@ -0,0 +1,50 @@
+<script type="ts" context="module">
+  export interface OnQueButtonClick {
+    event: MouseEvent;
+  }
+</script>
+
+<script type="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  let clazz: string = '';
+  export let border: boolean = false;
+  export { clazz as class };
+
+  const dispatcher = createEventDispatcher();
+
+  const onClick = (e: MouseEvent) => {
+    dispatcher('click', e);
+  };
+</script>
+
+<button
+  class="que-button-wrapper {border ? 'border' : ''} {clazz}"
+  on:click={e => onClick(e)}
+>
+  <slot />
+</button>
+
+<style type="scss">
+  .que-button-wrapper {
+    background-color: transparent;
+    border: 0;
+    transition: all 0.1s ease-in-out;
+    display: flex;
+    flex: 1 1 auto;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+      background: #eee;
+    }
+
+    &:active {
+      background: #fff;
+    }
+
+    &.border {
+      border: 1px solid rgba(0, 0, 0, 0.1);
+    }
+  }
+</style>
--- src/sub-toolbar/SubToolbar.svelte
+++ src/sub-toolbar/SubToolbar.svelte
@@ -1,5 +1,6 @@
 <script type="ts">
   import { useDispatch, useSelector } from '@/app/hooks';
+  import QueButton from '@/buttons/QueButton.svelte';
   import {
     currentQueueIndexSelector,
     currentQueueRangeSelector,
@@ -19,49 +20,100 @@
     dispatch(queueIndexSlice.actions.set($currentQueueIndex + 1));
   };
 
+  const onIndexClicked = (index: number) => {
+    dispatch(queueIndexSlice.actions.set(index));
+  };
+
   const onScaleDownClicked = () => {
-    dispatch(scaleSlice.actions.increase());
+    dispatch(scaleSlice.actions.decrease());
   };
 
   const onScaleUpClicked = () => {
-    dispatch(scaleSlice.actions.decrease());
+    dispatch(scaleSlice.actions.increase());
   };
 </script>
 
 <div id="subtoolbar">
-  <div class="subtoolbar-item btn prev">
-    <button on:click={onPrevClicked}>prev</button>
-  </div>
-  {#if $range}
-    {#each $range as index}
-      <div class="subtoolbar-item {index === $currentQueueIndex ? 'current' : ''}">
-        {index > -1 ? index + 1 : ''}
-      </div>
-    {/each}
-  {/if}
-  <div class="subtoolbar-item btn prev">
-    <button on:click={onNextClicked}>next</button>
+  <div class="subtoolbar-button-group">
+    <div class="subtoolbar-item">
+      <QueButton class="subtoolbar-button">
+        <i class="ri-arrow-go-back-line" />
+      </QueButton>
+    </div>
+    <div class="subtoolbar-item">
+      <QueButton class="subtoolbar-button">
+        <i class="ri-arrow-go-forward-line" />
+      </QueButton>
+    </div>
   </div>
 
-  <div>
-    <button on:click={onScaleDownClicked}>-</button>
-    <button on:click={onScaleUpClicked}>+</button>
+  <div class="subtoolbar-button-group">
+    <div class="subtoolbar-item">
+      <QueButton class="subtoolbar-button" on:click={() => onPrevClicked()}>
+        <i class="ri-arrow-left-line" />
+      </QueButton>
+    </div>
+    {#each $range as index}
+      <div class="subtoolbar-item">
+        <QueButton class="subtoolbar-button" on:click={() => onIndexClicked(index)}>
+          <span class="que-index {index === $currentQueueIndex ? 'current' : ''}">
+            {index > -1 ? index + 1 : ''}
+          </span>
+        </QueButton>
+      </div>
+    {/each}
+    <div class="subtoolbar-item">
+      <QueButton class="subtoolbar-button" on:click={() => onNextClicked()}>
+        <i class="ri-arrow-right-line" />
+      </QueButton>
+    </div>
+  </div>
+
+  <div class="subtoolbar-button-group">
+    <div class="subtoolbar-item">
+      <QueButton class="subtoolbar-button" on:click={() => onScaleDownClicked()}>
+        <i class="ri-subtract-line" />
+      </QueButton>
+    </div>
+
+    <div class="subtoolbar-item">
+      <QueButton class="subtoolbar-button" on:click={() => onScaleUpClicked()}>
+        <i class="ri-add-line" />
+      </QueButton>
+    </div>
   </div>
 </div>
 
 <style type="scss">
   #subtoolbar {
+    height: 40px;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     border-bottom: 1px solid rgb(0 0 0 / 10%);
 
     .subtoolbar-item {
-      margin: 10px;
-
-      &.current {
-        color: red;
-      }
+      height: 100%;
+      width: 40px;
+      display: flex;
     }
+  }
+
+  .subtoolbar-button-group {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .que-index {
+    color: black;
+    &.current {
+      color: red;
+    }
+  }
+
+  .subtoolbar-button {
+    height: 100%;
   }
 </style>
